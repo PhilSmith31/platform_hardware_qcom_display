38b993ac77c025a15387aaa20df844716760286a
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-11-14 19:42:30 +0100

    Gralloc: expose headers
    Needed for Gemini OSS Camera
    
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- msm8996/libgralloc/Android.mk
+++ msm8996/libgralloc/Android.mk
@@ -34,6 +34,8 @@ LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdgralloc\" -Wno-si
 LOCAL_CLANG                   := true
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps) $(kernel_deps)
 LOCAL_SRC_FILES               := gpu.cpp gralloc.cpp framebuffer.cpp mapper.cpp
+LOCAL_COPY_HEADERS_TO         := $(common_header_export_path)
+LOCAL_COPY_HEADERS            := gralloc_priv.h gr.h adreno_utils.h
 
 LOCAL_STATIC_LIBRARIES        := libgralloc1-adapter
 LOCAL_SHARED_LIBRARIES        += libsync
